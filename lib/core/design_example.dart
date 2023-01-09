6acc9078adb84a9e16340b8204661db32b8626c7
Anna Savostianova <annasavostianova@MacBook-Pro-Anna.local>
2023-01-09 21:01:04 +0200

    home background

---
+++ lib/core/design_example.dart
@@ -0,0 +1,7 @@
+class DesignStyleItem {
+  final String imagePath;
+  final String title;
+  final String? description;
+  DesignStyleItem(
+      {required this.title, required this.imagePath, this.description});
+}
---
+++ lib/core/instances.dart
@@ -0,0 +1,10 @@
+import 'package:animations/core/design_example.dart';
+
+final List<DesignStyleItem> designStyles = [
+  DesignStyleItem(
+      title: 'Minimalism', imagePath: 'assets/items/minimalism.jpg'),
+  DesignStyleItem(
+      title: 'Minimalism 2', imagePath: 'assets/items/minimalism2.jpg'),
+  DesignStyleItem(
+      title: 'Minimalism 3', imagePath: 'assets/items/minimalism3.jpg'),
+];
